e6b56913698531fe30a0bc334ccbeeb7279e6513
sufyan72562 <sufyan72562@gmail.com>
2026-05-16 23:08:09 +0500

    safe the session

--- app/dependencies.py
+++ app/dependencies.py
@@ -17,3 +17,5 @@ async def get_db() -> AsyncGenerator[AsyncSession, None]:
         except Exception:
             await session.rollback()
             raise
+        finally:
+            await session.close()
